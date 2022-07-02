062fbca01b1213a9e49f791a440d6ac634a80855
MihirNaik2001 <104674513+MihirNaik2001@users.noreply.github.com>
2022-07-02 16:04:00 +0530

    commit 5

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -5,8 +5,9 @@ const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const JWT_SECRET = "Mihirisagoodb$oy";
+const fetchuser = require('../middleware/fetchuser')
 
-//Create a User using:POST "/api/auth/createuser".No login required
+//ROUTE 1: Create a User using:POST "/api/auth/createuser".No login required
 router.post(
   "/createuser",
   [
@@ -47,7 +48,7 @@ router.post(
       //res.json(user);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some error occured.");
+      res.status(500).send("Internal server error occrured.");
     }
 
     //   .then(user => res.json(user))
@@ -57,5 +58,58 @@ router.post(
     //   });
   }
 );
+
+//ROUTE 2: Authenticate a User using:POST "/api/auth/login".No login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email.").isEmail(),
+    body("password", "Password cannot be blank.").exists(),
+  ],
+  async (req, res) => {
+    //If there are errors, return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials." });
+      }
+      const passwordCompare = await bcrypt.compare(password, user.password);
+      if (!passwordCompare) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials." });
+      }
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+      const authtoken = jwt.sign(data, JWT_SECRET);
+      res.json({ authtoken: authtoken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal server error occrured.");
+    }
+  }
+);
+
+//ROUTE 3: Get logged in user details using:POST "/api/auth/getuser".login required
+router.post("/getuser", fetchuser ,async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal server error occrured.");
+  }
+});
 
 module.exports = router;
